6cfadfd9e5e3a8b4825d91eb2b2fbf9d059876df
Matthew McKney <MMcKney@hedgecovest.local>
2021-11-09 16:15:09 -0500

    backspaced lines

--- src/components/CountHome.vue
+++ src/components/CountHome.vue
@@ -4,20 +4,21 @@
     >
       {{ $store.state.counter }}
   </h1>
-  <button @click="$store.dispatch('addCount')">+</button>
-  <button @click="$store.dispatch('subCount')">-</button>
-    <div>
+      <button @click="$store.dispatch('addCount')">+</button>
+      <button @click="$store.dispatch('subCount')">-</button>
+    <div class="container-a">
       {{ $store.state.counter }}
     <sup>2</sup> =
       {{ $store.getters.counterSquared }}
     </div>
-    <div>
+    <div class="container-b">
       {{ $store.state.counter }}
     <sup>3</sup> =
       {{ $store.getters.counterCubed }}
     </div>
     <div>
       <input 
+        class="input-modal"
         v-model="colorCode"
         placeholder="Enter color code" 
         type="text"
@@ -29,15 +30,29 @@
 export default {
     name: 'CountHome',
     computed: {
-        colorCode: {
-            get() {
-                return this.$store.state.colorCode
-            },
-            set(newColor) {
-                this.$store.dispatch('setColorCode', newColor)
-            }
+      colorCode: {
+        get() {
+          return this.$store.state.colorCode
+        },
+        set(newColor) {
+          this.$store.dispatch('setColorCode', newColor)
+        }
         }
     }
 }
 </script>
+
+<style scoped>
+.container-a {
+  font-size: 30px;
+}
+.container-b {
+  font-size: 30px;
+}
+.input-modal {
+  margin: 10px;
+  padding: 10px;
+  font-size: 30px;
+}
+</style>
 
